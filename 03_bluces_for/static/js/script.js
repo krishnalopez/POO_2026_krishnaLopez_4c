b0c3be123ceff08a806919aa5718d87de1b86f85
krishnalopez <krishnalopez@liceovvh.cl>
2026-03-12 11:10:01 -0300

    m

---
+++ 03_bluces_for/static/js/script.js
@@ -0,0 +1,159 @@
+/* 1. El Contador en Pantalla
+Selecciona el div con el id pantalla.
+Crea un ciclo for que vaya del 1 al 10.
+En cada vuelta del ciclo, agrega (concatena con +=) 
+el número actual seguido de un guion (ej: "1 - 2 - 3 - ") al párrafo.
+*/
+function contadorPantalla() {
+    let numeros = [];
+    const container = document.getElementById('resultadoContainer');
+    const result1 = document.getElementById('result1');
+    for (let i = 1; i <= 10; i++) {
+        numeros.push(i);
+    }
+    result1.textContent = `Contador: ${numeros.join(" - ")}`;
+    container.classList.remove('d-none');
+}
+
+
+/* 2. Lista de Asistencia Automática
+Tienes la siguiente lista de estudiantes:
+let curso = ["Ana", "Diego", "Sofía", "Matias"];
+Selecciona el párrafo pantalla y límpialo primero asignándole un texto vacío ("").
+Usa un ciclo for para recorrer el arreglo. En cada iteración,
+agrega el nombre del estudiante al párrafo, separándolos por una coma y un espacio.
+ */
+function asistenciaAutomatica() {
+    let curso = ["Ana", "Diego", "Sofía", "Matias"];
+    let resultado = [];
+    const container2 = document.getElementById('resultadoContainer2');
+    const result2 = document.getElementById('result2');
+    result2.textContent = "";
+    for (let i = 0; i < curso.length; i++) {
+        resultado.push(curso[i])
+    }
+    result2.textContent = `${resultado.join(", ")}`;
+    container2.classList.remove('d-none');
+
+}
+
+
+/* 
+3. Buscador de Aprobados (Escala 1 a 7)
+Tienes una lista con las notas finales de un curso: let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+Crea una variable contadorAprobados que inicie en 0.
+Recorre el arreglo con un for. Si la nota es 4.0 o superior, suma 1 al contador.
+Al terminar el ciclo, muestra en el párrafo pantalla: "Total de alumnos aprobados: [número]".
+ */
+function buscadorAprobados() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    let contadorAprobados = 0;
+    const container3 = document.getElementById('resultadoContainer3');
+    const result3 = document.getElementById('result3');
+
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 4) {
+            contadorAprobados++;
+        }
+    }
+    result3.textContent = `Contador: ${contadorAprobados}`;
+    container3.classList.remove('d-none');
+}
+
+
+/*4  El Filtro de Inventario
+Una tienda quiere mostrar solo los productos que están disponibles.
+let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+Recorre la lista con un ciclo for.*/
+function filtroInventario() {
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+    let resultado = [];
+    const container4 = document.getElementById('resultadoContainer4');
+    const result4 = document.getElementById('result4');
+    for (let i = 0; i < productos.length; i++) {
+        // ! diferente
+        if (productos[i] !== "Agotado") {
+            resultado.push(productos[i]);
+        }
+    }
+    result4.textContent = resultado.join(", ");
+    container4.classList.remove('d-none');
+}
+
+/*5.Sumando la Colecta
+Durante una actividad escolar se recolectaron distintos montos en dinero: let aportes = [1500, 2000, 500, 3000, 1000];
+Crea una variable totalRecaudado que inicie en 0.
+Usa un ciclo for para sumar cada uno de los aportes a la variable total.
+Al finalizar el ciclo, escribe en el párrafo: "La colecta reunió un total de: $[total]".
+ */
+function sumandoColecta() {
+    let aportes = [1500, 2000, 500, 3000, 1000];
+    let totalRecaudado = 0;
+    const container5 = document.getElementById('resultadoContainer5');
+    const result5 = document.getElementById('result5');
+    for (let i = 0; i < aportes.length; i++) {
+        totalRecaudado += aportes[i];
+    }
+    result5.textContent = `La colecta reunió un total de: ${totalRecaudado}.`;
+    container5.classList.remove('d-none');
+}
+
+/*6.Formateador de Nombres VIP
+Tienes una lista de usuarios: let asistentes = ["carlos", "MARIA", "pedro", "LUCIA"];
+Queremos mostrarlos en el HTML, pero destacando a algunos.
+Recorre el arreglo. Si el índice actual (i) es par (puedes usar i % 2 === 0), agrega la palabra " [VIP]" al lado de su nombre y muéstralo en el párrafo. Si es impar, muéstralo normal.
+ */
+function nombresVip() {
+    let asistentes = ["carlos", "MARIA", "pedro", "LUCIA"];
+    let resultado = [];
+    const container6 = document.getElementById('resultadoContainer6');
+    const result6 = document.getElementById('result6');
+    for (let i = 0; i < asistentes.length; i++) {
+        if (i % 2 === 0) {
+            resultado.push(`${asistentes[i]} [VIP]`);
+        } else {
+            resultado.push(`${asistentes[i]}`);
+        }
+    }
+    result6.textContent = resultado.join(" -");
+    container6.classList.remove('d-none');
+}
+
+/*7. El Buscador de Stock Específico
+Tienes un inventario: let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+Crea una variable articuloBuscado = "Cuaderno" y una variable vecesEncontrado = 0.
+Recorre la bodega con un for. Cada vez que el elemento de la lista sea igual al articuloBuscado, aumenta tu contador.
+Al final, inyecta en el párrafo: "El artículo [articuloBuscado] se encuentra [veces] veces en la bodega". 
+ */
+function stockEspecifico() {
+    let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+    let articuloBuscado = "Cuaderno";
+    let vecesEncontrado = 0;
+    const container7 = document.getElementById('resultadoContainer7');
+    const result7 = document.getElementById('result7');
+    for (let i = 0; i < bodega.length; i++) {
+        if (bodega[i] === articuloBuscado) {
+            vecesEncontrado++;
+        }
+    }
+    result7.textContent = `El artículo ${articuloBuscado} se encuentra ${vecesEncontrado} veces en la bodega`;
+    container7.classList.remove('d-none');
+}
+
+/* 8.  Generador de Párrafos de Advertencia
+Tienes una lista de temperaturas registradas en una sala de servidores: let temperaturas = [22, 24, 28, 35, 21, 38];
+Queremos mostrar alertas solo para las temperaturas peligrosas.
+Usa .innerHTML en lugar de .textContent en tu párrafo.
+*/
+function parrafosAdvertencia() {
+    let temperaturas = [22, 24, 28, 35, 21, 38];
+    const container8 = document.getElementById('resultadoContainer8');
+    const result8 = document.getElementById('result8');
+    for (let i = 0; i < temperaturas.length; i++) {
+        if (temperaturas[i] > 30) {
+            result8.innerHTML += `¡alerta temperaturas peligrosas de ${temperaturas[i]} grados. <br>!`
+        }
+    }
+    container8.classList.remove('d-none');
+}
+
